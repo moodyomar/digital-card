29d04cbfc433fcea2a5943da6f519726d25516e2
Mohammad Omar Shihab <lonak.secret@gmail.com>
2025-02-25 13:01:16 +0200

    make svc modal bigger

--- src/components/ServiceModal/ServiceModal.js
+++ src/components/ServiceModal/ServiceModal.js
@@ -9,7 +9,7 @@ export const ServiceModal = ({closeCalendarModal,isCalendarModalOpen}) => {
         <Modal isOpen={isCalendarModalOpen} onRequestClose={closeCalendarModal} className="modal" overlayClassName="overlay">
             <div className="modal-content">
                 {/* <h2>تعيين موعد اونلاين</h2> */}
-                <iframe src="https://qbmedia.setmore.com/" title="Schedule Appointment" scrolling="yes" width="100%" height="850" frameborder="0"></iframe>
+                <iframe src="https://qbmedia.setmore.com/" title="Schedule Appointment" scrolling="yes" width="100%" height="900" frameborder="0"></iframe>
                 {/* <button className="close-button" onClick={closeCalendarModal}>X</button> */}
             </div>
         </Modal>
